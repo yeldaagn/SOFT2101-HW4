966a0aef0f67c852b95d45d49b876d154d84da2b
Yelda Gün <yeldaagn@gmail.com>
2026-01-02 19:04:40 +0300

    satisfying requirement change three

--- hmk1/Main.java
+++ hmk1/Main.java
@@ -16,11 +16,12 @@ public class Main {
             second = temp;
         }
 
-     for (int i = first; i <= second; i++) {
-        if (i % 2 != 0) {
+    for (int i = first; i <= second; i++) {
+    if (i % 2 == 0) {
         System.out.println(i);
     }
 }
+
  
 
         scanner.close();
