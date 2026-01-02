7eff45379211ac04a5b3109d99353a05b7962df8
Yelda Gün <yeldaagn@gmail.com>
2026-01-02 18:54:14 +0300

    satisfying requirement one

---
+++ hmk1/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int first = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int second = scanner.nextInt();
+
+        if (first > second) {
+            int temp = first;
+            first = second;
+            second = temp;
+        }
+
+        for (int i = first; i <= second; i++) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+    }
+}
+
